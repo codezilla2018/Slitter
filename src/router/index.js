b3d78a5a3b265037dd258d91777bb00a5cfcc508
Harshana Serasinghe <harshanadabezt@gmail.com>
2018-05-20 09:59:06 +0530

    Slideshow route added

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,12 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+
+    {
+      path: '/slideshow',
+      name: 'Slideshow',
+      component: Slideshow
     }
   ]
 })
